a9b6c803655d95099de9191e2a7a0ed382d120ca
LukeDream <ruzhixu@qq.com>
2014-07-02 20:04:55 +0800

    ES 1.3-1.4

---
+++ ES1.4/ES1.4/源.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main ()
+{
+	int v1,v2;
+	std::cout<<"Enter two number :" <<std::endl;
+	std::cin>>v1>>v2;
+	std::cout<<v1 << " * "<< v2 <<" = "<< v1*v2<<std::endl;
+	system("pause");
+	return 0;
+}
